1c2e866077740d4418c3c9179acde7aaad26c49e
Gabriel Batista de Lima <dev.gabriel.lima@gmail.com>
2013-10-01 20:09:06 -0300

    First

---
+++ Account/ForgotPassword.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+
+namespace TrabalhoBernardi.Account
+{
+    public partial class ForgotPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MailMessage msg = new MailMessage("email", "email");
+            msg.Sender = new MailAddress("email");
+
+            System.Net.Mail.SmtpClient cli = new SmtpClient("smpt.gmail.com", 465);
+            cli.EnableSsl = true;
+            cli.DeliveryMethod = SmtpDeliveryMethod.Network;
+            cli.Credentials = new NetworkCredential("email", "senha");
+            cli.Send(msg);
+
+        }
+    }
+}
